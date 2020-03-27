dc93d4085fc2565ceee664510f3e37fc8e96709c
Excelsior <null@std.uestc.edu.cn>
2020-03-27 21:27:32 +0800

    gitignore added

--- intP.cc
+++ intP.cc
@@ -1,16 +1,14 @@
-auto intP(const TF1& func, double ll, double ul, double C, double epsrel = 1.e-3){
-	long long npx = (ul - ll)/epsrel + 1;
-	printf("%lld %f %f %f", npx, ll , ul, epsrel);
+auto intP(const TF1& func, double ll, double ul, double C, double epsilon = 1.e-3, double epsrel = 1.e-12){
+	size_t npx = (ul - ll)/epsilon + 1;
 	auto tgp = std::make_shared<TGraph>(npx);
 	double ax = ll;
 	tgp->SetPoint(0, ax, C);
-	for(long long ix = 1; ix != npx; ++ix){
-		tgp->SetPoint(ix, ax, const_cast<TF1&>(func).Integral(ll, ax) + C);
-		ax += epsrel;
+	for(size_t ix = 1; ix != npx; ++ix){
+		tgp->SetPoint(ix, ax, const_cast<TF1&>(func).Integral(ll, ax, epsrel) + C);
+		ax += epsilon;
 	}
 	auto lmb = [tgp](const double* x, const double* p){return tgp->Eval(x[0]);};
 	return std::make_shared<TF1>(func.GetName(), lmb, ll, ul, 0);
-	//return std::make_shared<TF1>(func.GetName(), [&](const double* x, const double* p){return tgp->Eval(x[0]);}, ll, ul);
 }
 
 auto intP(const TF1& func, double ll, double ul){
