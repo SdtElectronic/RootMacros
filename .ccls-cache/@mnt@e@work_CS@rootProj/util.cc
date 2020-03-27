eb9165103cac12de151dac1f8418861b731e44de
Excelsior <null@std.uestc.edu.cn>
2020-03-27 21:31:52 +0800

    removed ccls cache

--- .ccls-cache/@mnt@e@work_CS@rootProj/util.cc
+++
@@ -1,8 +0,0 @@
-double limP(const TF1& func, double ix){
-	double iy = func.Eval(ix);
-	while(isnan(iy) || isinf(iy)){
-		ix += numeric_limits<double>().epsilon();
-		iy = func.Eval(ix);	
-	}
-	return iy;
-}
