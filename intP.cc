4e356aa25730f73b81a50da3a17c0cfedc04054a
Excelsior <null@std.uestc.edu.cn>
2020-03-28 10:03:39 +0800

    Some dumb shit corrected

--- intP.cc
+++ intP.cc
@@ -1,5 +1,5 @@
-auto intP(const TF1& func, double ll, double ul, double C, double epsilon = 1.e-3, double epsrel = 1.e-12){
-	size_t npx = (ul - ll)/epsilon + 1;
+auto intP(const TF1& func, double ll, double ul, double C, double epsrel = 1.e-12){
+	/*size_t npx = (ul - ll)/epsilon + 1;
 	auto tgp = std::make_shared<TGraph>(npx);
 	double ax = ll;
 	tgp->SetPoint(0, ax, C);
@@ -7,7 +7,9 @@ auto intP(const TF1& func, double ll, double ul, double C, double epsilon = 1.e-
 		tgp->SetPoint(ix, ax, const_cast<TF1&>(func).Integral(ll, ax, epsrel) + C);
 		ax += epsilon;
 	}
-	auto lmb = [tgp](const double* x, const double* p){return tgp->Eval(x[0]);};
+	auto lmb = [tgp](const double* x, const double* p){return tgp->Eval(x[0]);};*/
+	auto lmb = [=](const double* x, const double* p){	
+		return const_cast<TF1&>(func).Integral(ll, x[0], epsrel) + C;};
 	return std::make_shared<TF1>(func.GetName(), lmb, ll, ul, 0);
 }
 
